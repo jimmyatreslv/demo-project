54e38ff5df2eda95918550f085b9cf86f5fea98d
Jimmy <jimmy@reslv.io>
2020-07-24 15:14:42 +0800

    add demo test case

--- src/main/java/com/example/demoproject/rest/HomeController.java
+++ src/main/java/com/example/demoproject/rest/HomeController.java
@@ -2,15 +2,16 @@ package com.example.demoproject.rest;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class HomeController {
     @Value("${app.version}")
     private String appVersion;
 
     @GetMapping("/")
-    public String greeting() {
-        return "hello home @" + appVersion;
+    public @ResponseBody String greeting() {
+        return "Hello, World @" + appVersion;
     }
 }
--- src/test/java/com/example/demoproject/DemoApplicationTests.java
+++ src/test/java/com/example/demoproject/DemoApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.demoproject;
 
+import com.example.demoproject.rest.HomeController;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	private HomeController controller;
+
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
